3844c8366b9a5486233cbeab0ba3b4aead84cf14
[Ruliaga] <ruliaga@mail.ru>
2022-06-16 23:58:13 +0500

    add NavigationDrawer

---
+++ app/src/main/java/ru/evgenysidorov/notepad/AboutFragment.java
@@ -0,0 +1,37 @@
+package ru.evgenysidorov.notepad;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class AboutFragment extends Fragment {
+
+    private TextView textView;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+
+}
--- app/src/main/java/ru/evgenysidorov/notepad/MainActivity.java
+++ app/src/main/java/ru/evgenysidorov/notepad/MainActivity.java
@@ -1,17 +1,23 @@
 package ru.evgenysidorov.notepad;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbarAndDrawer();
 
 
 
@@ -30,6 +37,60 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public void openAboutFragment() {
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .add(R.id.fragmentContainer,
+                                new AboutFragment()).commit();
+
+    }
+    private void initToolbarAndDrawer() {
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initDrawer(toolbar);
+    }
+
+
+
+
+    private void initDrawer(Toolbar toolbar) {
+// Находим DrawerLayout
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+// Создаем ActionBarDrawerToggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+// Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.about_drawer:
+                        openAboutFragment();
+                        return true;
+                    case R.id.exit_drawer:
+                            finish();
+                            return true;
+                } return false;
+            }
+        });
+    }
+
+   
 }
 
 
